cb3c70f7cf63142fb4380f5bc0aaaae344ad6fac
Kaushik <13kaushikk@gmail.com>
2024-03-16 16:47:23 +0530

    code refactor

--- authentication_service/src/services/query.rs
+++ authentication_service/src/services/query.rs
@@ -9,11 +9,7 @@ pub struct QueryRoot;
 
 #[juniper::object(Context = Context)]
 impl QueryRoot {
-    pub fn login(
-        &self,
-        context: &Context,
-        username: String,
-        password: String,
+    pub fn login(&self, context: &Context, username: String, password: String,
     ) -> Result<User, FieldError> {
         login::user_login(context, username, password)
     }
