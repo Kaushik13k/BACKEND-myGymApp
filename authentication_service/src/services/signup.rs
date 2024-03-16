9b2b7a707fc0b96f040cbbef5eac294d9078e8d5
Kaushik <13kaushikk@gmail.com>
2024-03-16 20:18:32 +0530

    added the user exist check for signup

--- authentication_service/src/services/signup.rs
+++ authentication_service/src/services/signup.rs
@@ -8,6 +8,8 @@ use diesel::prelude::*;
 use juniper::FieldError;
 use log::{error, info};
 
+use super::get_user;
+
 pub fn insert_user(context: &Context, new_user: Signup) -> Result<User, diesel::result::Error> {
     let connection = &mut context.db.establish_connection();
     let user = diesel::insert_into(users::users::table)
@@ -17,28 +19,50 @@ pub fn insert_user(context: &Context, new_user: Signup) -> Result<User, diesel::
 }
 
 pub fn user_signup(context: &Context, user_input: InputSignup) -> Result<User, FieldError> {
+    let username = user_input.username.clone();
     let password_hashed = hash_password(&user_input.password);
-    let new_user = Signup {
-        firstname: &user_input.firstname,
-        lastname: &user_input.lastname,
-        username: &user_input.username,
-        email: &user_input.email,
-        hash: &password_hashed.unwrap(),
-        phone_number: &user_input.phone_number,
-        age: &user_input.age,
-    };
-    match insert_user(context, new_user) {
-        Ok(user) => {
-            info!("The user was inserted successfully.");
-            Ok(user)
-        }
-        Err(_) => {
-            error!("Failed to insert user");
+    let user_data = get_user::get_user(context, username);
 
-            Err(FieldError::new(
-                "Failed to insert user",
+    match user_data {
+        Ok(_) => {
+            error!("User already exists");
+            return Err(FieldError::new(
+                "User already exists",
                 juniper::Value::null(),
-            ))
+            ));
+        }
+        Err(diesel::result::Error::NotFound) => {
+            info!("No user found");
+            let new_user = Signup {
+                firstname: &user_input.firstname,
+                lastname: &user_input.lastname,
+                username: &user_input.username,
+                email: &user_input.email,
+                hash: &password_hashed.unwrap(),
+                phone_number: &user_input.phone_number,
+                age: &user_input.age,
+            };
+            match insert_user(context, new_user) {
+                Ok(user) => {
+                    info!("The user was inserted successfully.");
+                    Ok(user)
+                }
+                Err(_) => {
+                    error!("Failed to insert user");
+
+                    Err(FieldError::new(
+                        "Failed to insert user",
+                        juniper::Value::null(),
+                    ))
+                }
+            }
+        }
+        Err(e) => {
+            error!("There was an error in the while getting token: {:?}", e);
+            return Err(FieldError::new(
+                format!("Error: {:?}", e),
+                juniper::Value::null(),
+            ));
         }
     }
 }
