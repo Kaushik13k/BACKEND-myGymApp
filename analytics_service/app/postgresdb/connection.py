5319ee5ba516c74747f71965ef64428874f2dfdb
Kaushik <13kaushikk@gmail.com>
2024-04-21 18:36:58 +0530

    added env vars

--- analytics_service/app/postgresdb/connection.py
+++ analytics_service/app/postgresdb/connection.py
@@ -2,11 +2,11 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from enums.envs import Env
+from enums.env import EnvironmentVariables
 
 Base = declarative_base()
 
-DATABASE_URL = Env.POSTGRES_CONNECTION_URL.value
+DATABASE_URL = EnvironmentVariables.POSTGRES_CONNECTION_URL.value
 
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
